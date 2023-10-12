ebebd047b84805579e71397ce3ba5616ba24fd29
agussnicoletti <agus.n2001@gmail.com>
2023-10-11 21:54:50 -0300

    se agrega el segundo apellido

--- agustina_nicoletti.py
+++ agustina_nicoletti.py
@@ -1,7 +1,11 @@
 from os import system
 system ("cls")
 
+
+
+
 nombre = "Agustina"
 apellido = "Nicoletti"
+segundo_apellido = "Elgart"
 
-print ("Mi nombre es" ,nombre, apellido)
+print ("Mi nombre es" ,nombre, apellido, segundo_apellido)
